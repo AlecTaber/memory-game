f8eaa6a0de49dffc077c27c6b73b62b5763c4ea0
Alec Taber <alectaber12@gmail.com>
2024-07-07 13:09:39 -0700

    add countdown function in JS

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-// JvaScript
+const timerEl = document.getElementById('countdown');
+const mainEl = document.getElementById('main');
+
+function countdown() {
+    let timeLeft = 5;
+    let message = "Time's Up";
+
+    const timeInterval = setInterval(function () {
+
+      if (timeLeft > 1) {
+        timerEl.textContent = timeLeft
+        timeLeft--;
+
+      } else {
+        timerEl.textContent = '';
+        clearInterval(timeInterval);
+        displayMessage();
+      }
+    }, 1000);
+  }
