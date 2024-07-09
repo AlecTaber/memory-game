deeeea5597a8601240d0df41a314f276be9ffb25
Alec Taber <alectaber12@gmail.com>
2024-07-08 19:29:28 -0700

    add play button

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
 const timerEl = document.getElementById('countdown');
 const mainEl = document.getElementById('main');
-
 function countdown() {
     let timeLeft = 5;
-    let message = "Time's Up";
-
     const timeInterval = setInterval(function () {
-
-      if (timeLeft > 1) {
-        timerEl.textContent = timeLeft
-        timeLeft--;
-
-      } else {
-        timerEl.textContent = '';
-        clearInterval(timeInterval);
-        displayMessage();
-      }
+        if (timeLeft > 0) {
+            timerEl.textContent = timeLeft;
+            timeLeft--;
+        } else {
+            timerEl.textContent = "Match!";
+            clearInterval(timeInterval);
+            // We'll add more code here to handle what happens when the timer reaches 0
+        }
     }, 1000);
-  }
+}
+const playButton = document.querySelector('.btn-primary.click');
+playButton.addEventListener('click', countdown);
